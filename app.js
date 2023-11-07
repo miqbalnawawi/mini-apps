0ed7902800b9c4a52e5de2abca578ee05f98e4f1
miqbalnawawi <miqbalnawawi11@gmail.com>
2023-11-07 18:32:34 +0700

    add app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ App({
   onLaunch(options) {
     // 第一次打开
     // options.query == {number:1}
-    console.info('App onLaunch 2');
+    console.info('App onLaunch 23');
   },
   onShow(options) {
     // 从后台被 scheme2 重新打开
